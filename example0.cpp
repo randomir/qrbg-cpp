9debbe4650bc8dd3072ff62edd8f7b57ca8e8633
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2013-02-04 01:36:32 +0100

    tabs to spaces

--- example0.cpp
+++ example0.cpp
@@ -1,22 +1,22 @@
-#include <stdio.h>		// printf
+#include <stdio.h>      // printf
 #include "QRBG.h"
 
 int main(int argc, char* argv[]) {
-	QRBG rndService;
-	rndService.defineServer("random.irb.hr", 1227);
-	rndService.defineUser("username", "password");
+    QRBG rndService;
+    rndService.defineServer("random.irb.hr", 1227);
+    rndService.defineUser("username", "password");
 
-	// acquire some of supported types: byte, integer, long integer, float and double
-	printf("QRBG service returned byte: %x \n", rndService.getByte());
-	printf("QRBG service returned integer: %x \n", rndService.getInt());
-	printf("QRBG service returned long integer: %x \n", rndService.getLongInt());
-	printf("QRBG service returned float: %f \n", rndService.getFloat());
-	printf("QRBG service returned double: %f \n", rndService.getDouble());
+    // acquire some of supported types: byte, integer, long integer, float and double
+    printf("QRBG service returned byte: %x \n", rndService.getByte());
+    printf("QRBG service returned integer: %x \n", rndService.getInt());
+    printf("QRBG service returned long integer: %x \n", rndService.getLongInt());
+    printf("QRBG service returned float: %f \n", rndService.getFloat());
+    printf("QRBG service returned double: %f \n", rndService.getDouble());
 
-	// acquire arbitrary number of random bytes
-	const int size = 313;
-	byte buffer[size];
-	printf("Requesting %d random bytes.. ", size);
-	rndService.getBytes(buffer, size);
-	printf("Received.\n");
+    // acquire arbitrary number of random bytes
+    const int size = 313;
+    byte buffer[size];
+    printf("Requesting %d random bytes.. ", size);
+    rndService.getBytes(buffer, size);
+    printf("Received.\n");
 }
